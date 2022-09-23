cbe794ffc6859009229fc9981baf6fdf1229181a
vishal035 <kumarvishal880288@gmail.com>
2022-09-23 15:14:41 +0530

    Project Init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className=" relative flex flex-col items-center">
+      <header className="relative flex flex-col top-10 items-center">
+        <img src={logo} className="w-96 h-96" alt="logo" />
+        <p className="font-normal text-3xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="font-medium text-xl mt-2"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
